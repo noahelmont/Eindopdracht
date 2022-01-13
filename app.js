318e7f24bcb00f1e91058ea3377b4fae289838bb
noah elmont <2047541@talnet.nl>
2022-01-13 11:34:43 +0100

    eindopdracht

--- app.js
+++ app.js
@@ -1,7 +1,11 @@
-const menu = document.querySelector('#mobile-menu')
-const menulinks = document.querySelector('.navbar_menu')
+const menu = document.querySelector('#mobile-menu');
+const menuLinks = document.querySelector('.navbar_menu');
+const navLogo = document.querySelector('#navbar_logo');
 
-menu.addEventListener('click', function() {
-    menu.classList.toggle('is-active');
-    menulinks.classList.toggle('active');
-}
+// Display Mobile Menu
+const mobileMenu = () => {
+  menu.classList.toggle('is-active');
+  menuLinks.classList.toggle('active');
+};
+
+menu.addEventListener('click', mobileMenu);
